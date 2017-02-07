b63f6e7ac64b3cbcca2346314a1a7db902655622
firegood007 <firegood@163.com>
2017-02-08 00:09:55 +0800

    no message

--- app/js/module/agents/view.js
+++ app/js/module/agents/view.js
@@ -1,5 +1,6 @@
 define(['jquery'], function($) {
     'use strict';
+    
     var view = function(){
       
     }
--- app/js/router.js
+++ app/js/router.js
@@ -1,11 +1,16 @@
-define(['director'], function(Router) {
+define(['director','jquery'], function(Router,$) {
     'use strict';
     var pages = ['agents'], // 模块名称
-        pageRegister = {}; // 模块注册 形如{ 'login' : function(){} }
+        pageRegister = {     // 模块注册 形如{  }
+            noDevelop: function(){   //未开发
+                var html = ['<h1>No development</h1>','<div>Stay tuned for</div>'].join('');
+                $('#page').html(html)
+            }
+        }; 
         /**
-         * [description] 使用闭包生成 pageRegister
+         * [description] 使用闭包生成动态生成 pageRegister
          */
-        pages.forEach(function(val,i) {
+        pages.forEach(function(val) {
             pageRegister[val] = (function(){
                 return function(){
                             require([val], function(page) {
@@ -17,9 +22,9 @@ define(['director'], function(Router) {
         // 返回一个router实例。
     return  Router({
                 '/'          : pageRegister.agents,
-                '/dashboard' : pageRegister.dashboard,
-                '/myCruise'  : pageRegister.myCruise,
+                '/dashboard' : pageRegister.noDevelop,
+                '/myCruise'  : pageRegister.noDevelop,
                 '/agents'    : pageRegister.agents,
-                '/help'      : pageRegister.help
+                '/help'      : pageRegister.noDevelop
             });
 })
